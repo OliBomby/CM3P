cd4e5bbb847897ac1a5441ca240d638df99c823f
OliBomby <olivierschipper@gmail.com>
2025-10-22 08:45:40 +0200

    Update test_classify.py

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -4,21 +4,24 @@ from cm3p import CM3PForBeatmapClassification
 from cm3p.processing_cm3p import CM3PProcessor
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-# save_path = r"saved_logs/train_v7_classifier/trainer_output/checkpoint-10000"
+# save_path = r"../saved_logs/train_v7_classifier/trainer_output/checkpoint-10000"
 # ranked_threshold = 0.8
-save_path = r"saved_logs/train_v7_classifier2/trainer_output/checkpoint-10000"
+save_path = r"../saved_logs/train_v7_classifier2/trainer_output/checkpoint-10000"
 ranked_threshold = 0.4
 
 processor = CM3PProcessor.from_pretrained(save_path)
 model = CM3PForBeatmapClassification.from_pretrained(save_path, torch_dtype=torch.bfloat16, device_map=device, attn_implementation="sdpa")
 
-# audio = r"resources/audio.mp3"
-# beatmap = r"resources/Denkishiki Karen Ongaku Shuudan - Aoki Kotou no Anguis (OliBomby) [Ardens Spes].osu"
-# audio = r"resources/audio2.mp3"
-# beatmap = r"resources/POLKADOT STINGRAY - Otoshimae (moph) [Mindmaster's Extra].osu"
-audio = r"resources/audio3.mp3"
-# beatmap = r"resources/Mitsuki Nakae - ANTICLOCK TEA-PARTY.osu"
-beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\beatmap-638965817922162241-ANTICLOCK TEA-PARTY\Mitsuki Nakae - ANTICLOCK TEA-PARTY (Mapperatorinator) [Mapperatorinator V30].osu"
+# audio = r"..\resources/audio.mp3"
+# beatmap = r"..\resources/Denkishiki Karen Ongaku Shuudan - Aoki Kotou no Anguis (OliBomby) [Ardens Spes].osu"
+# audio = r"..\resources/audio2.mp3"
+# beatmap = r"..\resources/POLKADOT STINGRAY - Otoshimae (moph) [Mindmaster's Extra].osu"
+audio = r"..\resources/audio3.mp3"
+# beatmap = r"..\resources/Mitsuki Nakae - ANTICLOCK TEA-PARTY.osu"
+# beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\beatmap-638965817922162241-ANTICLOCK TEA-PARTY\Mitsuki Nakae - ANTICLOCK TEA-PARTY (Mapperatorinator) [Mapperatorinator V30].osu"
+# beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\beatmap-638965817922162241-ANTICLOCK TEA-PARTY\beatmapa6fab5441dab43569157deee509de435.osu"
+# beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\beatmap-638965817922162241-ANTICLOCK TEA-PARTY\beatmap29f409a21875483ead1799ef778328e4.osu"
+beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\beatmap-638965817922162241-ANTICLOCK TEA-PARTY\beatmap17ea5e3d365948f9b4996b6900ccffdd.osu"
 # beatmap = r"C:\Users\Olivier\AppData\Local\osu!\Songs\1848363 garnet feat F9 - hauynite\garnet feat. F9 - hauynite (Shurelia) [rumination].osu"
 
 
